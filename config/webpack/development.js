a66cd16407b9077dabc49406f414ff3b136241bc
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-03-30 10:23:42 -0300

    chore: update views admin

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,16 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const { merge } = require('webpack-merge');
+const environment = require('./environment');
 
-const environment = require('./environment')
+// Define o ambiente de desenvolvimento
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-module.exports = environment.toWebpackConfig()
+// Carrega a configuração de ambiente do webpack e mescla com configurações específicas de desenvolvimento, se necessário
+module.exports = merge(environment.toWebpackConfig(), {
+    // Configurações específicas de desenvolvimento aqui
+
+    // Configuração do ambiente Node.js
+    node: {
+        __dirname: false,   // Desativa o uso de __dirname
+        __filename: false,  // Desativa o uso de __filename
+    }
+});
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Admin, type: :model do
-  it "admin is valid" do
-    admin = Admin.new(email: "admin@admin.com", password; "123456")
+  it "1. admin is valid" do
+   admin = Admin.new(email: "admin@admin.com", password: "123456")
 
-    expect(admin.email).to eql "admin@admin.com"
+   expect(admin.email).to eql "admin@admin.com"
+   expect(admin).to be_valid
+  end
 
+  it "2. admin is NOT valid" do
+    admin = Admin.new(email: "admin@admin.com")
 
-
-
+    expect(admin).to_not be_valid
   end
 end
