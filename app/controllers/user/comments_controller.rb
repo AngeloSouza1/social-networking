cbdd8070519d5561613be2ccf29b4c3fc8ed1045
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-05 09:32:03 -0300

    chore: ajuste em java, para atualizacao post p2

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -13,12 +13,17 @@ class User::CommentsController < UserController
   end
 
   def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @post = @comment.post
 
+    if @comment.destroy
+      respond_to do |format|
+        format.js { render "destroy" }
+      end
+    end
   end
 
-
   private
-
   def form_params
     params.require(:comment).permit(:body, :post_id)
   end
